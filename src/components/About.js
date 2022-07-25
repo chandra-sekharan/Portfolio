a6bb235111e735ffd38c8f2a0a1b97d2f1cb2a63
chandra-sekharan <chandrsekharan.cs@gmail.com>
2022-07-25 06:38:49 +0530

    second state

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import './styles/App.css';
 
 const About = () =>{
     return(
-        <div className="About">
+        <div className="About" id="about">
           <h1>About Me</h1>
           <div className="para">
            <p>Hello! My name is Chandra Shekar and I'm a Self taught Full Stack Web Developer from India.<br/>Currently Studying Under Graduation in Andhra pradesh. <br/>I started my web development through online courses by learning<br/> <a>HTML CSS JavaScript PHP and React and Building side projects.</a><br/>And now I'm Started learning Web3.0 decentralised technology to built apps on Block chain.</p>
