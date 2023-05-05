f005809ad97535386f4e57a9bebbd91040591a92
Chandra Shekar <80117733+chandra-sekharan@users.noreply.github.com>
2023-05-05 13:38:22 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -30,25 +30,25 @@ const Project = ()=>{
                 </div>
             </div>
             <div className="project-display">
-            <a href="https://weather-app-cs.herokuapp.com/" target="_blank" rel="noreferrer"><img src={weather} alt="" className="otherimages" /></a>
+            <a href="https://calculator-cs.netlify.app/" target="_blank" rel="noreferrer"><img src={weather} alt="" className="otherimages" /></a>
                 
                 <div className="discript">
-                <p>This is a Weather React Application made using WeatherAPI and React</p>
+                <p>This is a Calculator React Application made using React</p>
                 </div>
                 
                 <div className="pro-name">
-                    <h6><a href="https://weather-app-cs.herokuapp.com/" target="_blank" rel="noreferrer">Weather</a></h6>
+                    <h6><a href="https://calculator-cs.netlify.app/" target="_blank" rel="noreferrer">Calculator</a></h6>
                 </div>
                 
             </div>
             <div className="project-display">
-            <a href="https://cs-notes-app.herokuapp.com/" target="_blank" rel="noreferrer"><img src={notes} alt="" className="otherimages" /></a>
+            <a href="https://note-management-crud.netlify.app/" target="_blank" rel="noreferrer"><img src={notes} alt="" className="otherimages" /></a>
                 
                 <div className="discript">
                 <p>This is a Notes React Application made using React </p>
                 </div>                
                 <div className="pro-name">
-                    <h6><a href="https://cs-notes-app.herokuapp.com/" target="_blank" rel="noreferrer">Notes</a></h6>
+                    <h6><a href="https://note-management-crud.netlify.app/" target="_blank" rel="noreferrer">Note Application</a></h6>
                 </div>
               
             </div>
